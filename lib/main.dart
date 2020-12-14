ff8fe508c16fca3a1b1b545279f9abff2f451d1d
Abdul Mtoro III <abdulmtoro1997@gmail.com>
2020-12-14 14:42:00 -0800

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,30 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  //fixing async issue on main()..
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // final Color darken = Color(0XFF121212);
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Creator',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.pink,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Meme Creator v1.0'),
     );
   }
 }
@@ -29,39 +39,53 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  File _image;
+  File _createdMeme;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Center(
+              child: Text(
+            widget.title,
+          )),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            // Respond to button press
+          },
+          icon: Icon(Icons.add_a_photo_outlined),
+          label: Text('Add an Image'),
+        ),
+        body: new SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
+          child: Stack(children: <Widget>[
+            _image != null
+                ? Image.file(_image)
+                : Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      height: 350.0,
+                      decoration: new BoxDecoration(
+                          color: Colors.black.withOpacity(.2),
+                          // borderRadius: BorderRadius.only(
+                          //   // topRight: Radius.circular(30.0),
+                          //   // bottomLeft: Radius.circular(30.0),
+                          // ),
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Center(
+                        child: Icon(
+                          Icons.photo_camera,
+                          color: Colors.black26,
+                          size: 38.0,
+                        ),
+                      ),
+                    ),
+                  ),
+          ]),
+        ));
+
+    // This trailing comma makes auto-formatting nicer for build methods.
   }
 }
